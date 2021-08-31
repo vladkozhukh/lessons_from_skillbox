2bc44af78a59b316fbb03c49a801a81d744a5e5a
Kozhukh Vladislav <vlad32007@mail.ru>
2021-08-31 23:01:12 +0300

    add lesson 15.10 ver 1.0

---
+++ 15_10_Homework/app/src/main/java/com/example/fragmentsviewpager/ArticleType.kt
@@ -0,0 +1,12 @@
+package com.example.fragmentsviewpager
+
+enum class ArticleType (
+    val tag: String
+        ) {
+    Sport("Спорт"),
+    Health("Здоровье"),
+    Business("Бизнес"),
+    Auto("Автомобили"),
+    Technologies("Техгологии"),
+    People("Люди")
+}
