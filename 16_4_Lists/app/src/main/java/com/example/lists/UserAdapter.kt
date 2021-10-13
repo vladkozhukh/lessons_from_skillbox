85d270a635e9f6a1849e81e28351bb5bef08f131
Kozhukh Vladislav <vlad32007@mail.ru>
2021-10-13 22:37:38 +0300

    add lesson 16.4

--- 16_4_Lists/app/src/main/java/com/example/lists/UserAdapter.kt
+++ 16_4_Lists/app/src/main/java/com/example/lists/UserAdapter.kt
@@ -6,17 +6,25 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.text.FieldPosition
 
 class UserAdapter(
-    private val users: List<User>,
+    // 2.1. функция которая принимает position -> на выход ничего не производить и копируем в class Holder (..., сюда)
+    private val onItemClicked: (position: Int) -> Unit
 ) : RecyclerView.Adapter<UserAdapter.Holder>() {
+
+    // 1.4. придаем ему пустой список "... = emptyList()"
+    private var users: List<User> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         //        val inflater = LayoutInflater.from(parent.context)
         //        val view = inflater.inflate(R.layout.item_user,parent, false)
         //        return Holder(view)
         // упрощается через ViewExtensions.kt
-        return Holder(parent.inflate(R.layout.item_user))
+        return Holder(
+            parent.inflate(R.layout.item_user),
+            onItemClicked
+        ) // 2.4. "onItemClicked" передаем при создании
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
@@ -24,18 +32,37 @@ class UserAdapter(
         holder.bind(user)
     }
 
+    // 1.5. метод для обновления списка, который принимает новый список пользователей
+    fun updateUsers(newUsers: List<User>) {
+        users = newUsers
+        // 1.6. оповещение адаптера об изменении списка
+        // notifyDataSetChanged() -> 3.1 убираем для плавного добавления и удаления
+
+    }
+
     override fun getItemCount(): Int = users.size
 
-    class Holder(view: View) : RecyclerView.ViewHolder(view){
+    class Holder(
+        view: View,
+        // 2.2 копируем функцию
+        onItemClicked: (position: Int) -> Unit
+    ) : RecyclerView.ViewHolder(view) {
         private val nameTextView: TextView = view.findViewById(R.id.nameTextView)
         private val ageTextView: TextView = view.findViewById(R.id.ageTextView)
         private val developerTextView: TextView = view.findViewById(R.id.developerTextView)
         private val avatarImageView: ImageView = view.findViewById(R.id.avatarImageView)
 
-        fun bind(user: User){
-            nameTextView.text=user.name
-            ageTextView.text="Возраст = ${user.age}"
-            developerTextView.visibility = if(user.isDeveloper) View.VISIBLE else View.GONE
+        init {
+            view.setOnClickListener {
+                // 2.3 действие при нажатии
+                onItemClicked(adapterPosition)
+            }
+        }
+
+        fun bind(user: User) {
+            nameTextView.text = user.name
+            ageTextView.text = "Возраст = ${user.age}"
+            developerTextView.visibility = if (user.isDeveloper) View.VISIBLE else View.GONE
 
             Glide.with(itemView)
                 .load(user.avatarLink)
--- 16_4_Lists/app/src/main/java/com/example/lists/UserListFragment.kt
+++ 16_4_Lists/app/src/main/java/com/example/lists/UserListFragment.kt
@@ -8,9 +8,9 @@ import kotlinx.android.synthetic.main.fragment_user_list.*
 import kotlin.random.Random
 import kotlin.random.nextInt
 
-class UserListFragment: Fragment(R.layout.fragment_user_list) {
+class UserListFragment : Fragment(R.layout.fragment_user_list) {
 
-    private val users = listOf(
+    private var users = listOf(
         User(
             "Иван (Мортал) Иванов",
             "https://meragor.com/files/styles//220_220_bottom_wm/_mortal_kombat_0.jpg",
@@ -41,17 +41,56 @@ class UserListFragment: Fragment(R.layout.fragment_user_list) {
         )
     )
 
+    // 1.7 сохранение адаптера в поле фрагмента
+    private var userAdapter: UserAdapter? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initList()
+        // обработка нажатия на кнопку 1.1
+        addFab.setOnClickListener { addUser() }
+        // 1.11 вызываем updateUsers при первом отображении списка
+        userAdapter?.updateUsers(users)
+        // 3.2. прописываем для оповещения изменения списка
+        // userAdapter?.notifyDataSetChanged()
+        // другой вариант прописать
+        userAdapter?.notifyItemRangeChanged(0, users.size)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        userAdapter = null
     }
 
     private fun initList() {
+        // 1.8 инициализируем userAdapter
+        userAdapter =
+            UserAdapter { position -> deleteUser(position) } // 2.6 удаление в листе по определенной позиции
         with(userList) {
-            adapter = UserAdapter((users + users).shuffled())
+            adapter = userAdapter // 1.9 -> addUser
             layoutManager = LinearLayoutManager(requireContext())
             setHasFixedSize(true)
         }
+    }
+
+    // 2.5 метод удаления deleteUser
+    private fun deleteUser(position: Int) {
+        // 2.7 фильтрация списка при нажатии, позиция не ровна позиции на которую нажали
+        users = users.filterIndexed { index, user -> index != position }
+        // 2.8 переустановим новый список в адаптер
+        userAdapter?.updateUsers(users)
+        userAdapter?.notifyItemRemoved(position) // 3.4 удаление той позиции на которую нажали
+    }
+
+    // 1.2 метод создания addUser
+    private fun addUser() {
+        // случайный пользователь в первую строку
+        val newUser = users.random()
+        users = listOf(newUser) + users // далее см. 1.3 см.адаптер
+        userAdapter?.updateUsers(users) // 1.10 передаем новый список пользователей
+        userAdapter?.notifyItemInserted(0) // 3.3 добавление на первую строку
+
+        userList.scrollToPosition(0) // 3.5 скролинг к добавленой позиции
     }
 
 }
