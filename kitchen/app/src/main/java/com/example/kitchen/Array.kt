9622ca3d63e4906471f16ae49bd8c5e0297d085e
vlad3 <vlad32007@mail.ru>
2021-12-01 16:57:18 +0300

    add lesson 3.2

---
+++ kitchen/app/src/main/java/com/example/kitchen/Array.kt
@@ -0,0 +1,32 @@
+package com.example.kitchen
+// Array-массив и метод arrayOf. Метод arrayOf создаёт массив из переданных значений
+
+fun main() {
+    val array = arrayOf("one", "two", "three")
+
+    for (user in array){
+        println(user)
+    }
+
+    array.forEach { println(it) }
+
+    // Метод foreach/foreachIndexed.
+    // Методы foreach — это лямбда-функция, которая перебирает значение из коллекции и выполняет для них переданные действия
+
+    array.forEachIndexed { index, user -> println("$index -> $user") }
+    // Обращение по индексу
+    val firstElement = array[0] /* первый элемент в списке */
+    val lastElement = array[array.lastIndex] /* послдений элемент в списке */
+
+    // замена элемента в списке
+    array[0] = "New one"
+    array.forEach { println(it) }
+
+    // Метод emptyArray и класс Array
+    // пустой список нужно обязательно указывать тип, класс и т.д. в <...>
+    val emptyArray = emptyArray<String>()
+
+    val largeArray = Array(100){size -> "Index $size"}
+    largeArray.forEach { println(it)}
+
+}
---
+++ kitchen/app/src/main/java/com/example/kitchen/Statements.kt
@@ -0,0 +1,20 @@
+package com.example.kitchen
+// Expression-выражение.
+// Набор значений, переменных, операторов и функций, которые вычисляются для того, чтобы произвести другое значение
+
+// Statement-инструкция.
+// Элемент программы, выражающий действия, необходимые для выполнения
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+fun main() {
+    // Цепочка вызовов
+    calculateRandomInt()
+        .takeIf { it > 2 } /* елси it <= 2 -> it = null */
+        ?.let { print("Number > 2") /* если it > 2 -> "текст" */ }
+}
+
+fun calculateRandomInt(): Int {
+    return Random.nextInt(0 until 10)
+}
