75753344243bdadee536705ce87bf479d217e99b
Kozhukh Vladislav <vlad32007@mail.ru>
2021-12-02 22:33:15 +0300

    add lesson 4.2 collection

---
+++ kitchen/app/src/main/java/com/example/kitchen/Map.kt
@@ -0,0 +1,64 @@
+package com.example.kitchen
+
+fun main() {
+    val pair1 = Pair("key", "value")
+    val pair2 = 1 to 2
+
+    println("${pair1.first}, ${pair1.second}")
+    println(pair2)
+
+    val map = mapOf(
+        "1" to "+375-111-11-11",
+        "1" to "+375-NEW-11-11", /*перезапись ключа "1" */
+        "2" to "+375-222-22-22",
+        "3" to "+375-333-33-33",
+    )
+
+    println(map["1"])
+    println(map["3"])
+
+    // изначально map immutable, длеаем mutable:
+    val mutableMap = map.toMutableMap()
+
+    mutableMap["1"] = "111" /* замена 1го ключа */
+    mutableMap["11"] = "111-11-11" /* добавление ключа */
+    mutableMap.put("new key", "new value") /* добавление ключа */
+    mutableMap.put("new key2", "new value2") /* добавление ключа */
+
+    mutableMap.remove("new key2") /* удаление по ключу */
+
+    println("$mutableMap")
+
+    // создание сразу mutable map:
+    val mutableMap2 = mutableMapOf("one" to "new one")
+
+    // перевод обратно в immutable map (list, set):
+    mutableMap2.toMap()
+
+    // Функции sortedMapOf и hashMapOf
+    val sortedMap = sortedMapOf(
+        "1" to "11",
+        "3" to "33",
+        "4" to "44",
+        "2" to "22")
+    println(sortedMap)
+
+    val hashMap = hashMapOf(
+        "1" to "one",
+        "3" to "three",
+        "4" to "four",
+        "2" to "two")
+    println(hashMap)
+
+    println(hashMap.keys) /* только ключи */
+    println(hashMap.values) /* только значения по ключу */
+
+    // вывод ключа в списке ключей:
+    for (key in hashMap.keys){
+        println("$key, value = ${hashMap.keys} ")
+    }
+
+    hashMap.forEach{ entry-> entry.key
+    entry.value
+    }
+}
---
+++ kitchen/app/src/main/java/com/example/kitchen/Set.kt
@@ -0,0 +1,46 @@
+package com.example.kitchen
+
+fun main() {
+    // Set-множества. Множество уникальных значений
+    val immutableSet = setOf(1, 2, 3, 3, 4, 5, 6, 7, 7, 8)
+    println(immutableSet)
+
+    // union (объединение уникальных значений двух списков)
+    val unionSet = setOf(1, 2, 3).union(setOf(1, 4, 5))
+    println(unionSet + "union")
+
+    // subtract (возвращает элементы, которые есть в одном списке, но нет в другом)
+    val subtractSet = setOf(1, 2, 3).subtract(setOf(1, 4, 5))
+    println(subtractSet + "subtract")
+
+    // intersect (объединение двух списков)
+    val intersect = setOf(1, 2, 3).intersect(setOf(1, 4, 5))
+    println(intersect + "intersect")
+
+    val naturalOrderSet = sortedSetOf(1, 3, 0, -1, 10)
+    println(naturalOrderSet + "sortedSetOf 1..10")
+    // Метод descendingSet (расположит значения в обратном порядке)
+    println(naturalOrderSet.descendingSet() + "descendingSet 10..1")
+
+    // делаем его изменяемым .toMutableSet чтобы добавить в него индексы и элементы
+    val naturalMutableSet = sortedSetOf(1, 3, 0, -1, 10).toMutableSet()
+    naturalMutableSet.add(5)
+    /* или */
+    mutableListOf(1, 2, 3).add(4)
+
+    // Метод hashSetOf изначално mutable
+
+    val hashSet = hashSetOf(1,2,3)
+
+    // Методы add, remove, contains
+    hashSet.add(0)
+    hashSet.remove(2)
+
+    // содержиться ли в списке "1"
+    hashSet.contains(1) /* 1ой  вариант данной записи */
+    println(hashSet.contains(1)) /* true */
+    // содержиться ли в списке "2"
+    2 in hashSet /* 2ой  вариант данной записи */
+    println(2 in hashSet) /* false */
+
+}
