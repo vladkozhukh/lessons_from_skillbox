922b2f1266e735a87cf8951a82fdc057f3f645f9
vlad3 <vlad32007@mail.ru>
2022-01-12 13:30:28 +0300

    add module 8 zeppelin

--- kitchen/app/src/main/java/com/example/kitchen/module9/main.kt
+++ kitchen/app/src/main/java/com/example/kitchen/module9/main.kt
@@ -7,21 +7,46 @@ fun main() {
 //    println("capacity - ${train.capacity} and max weight - ${train.maxWeight}")
 //    train.move()
 
-    val boeing = Boeing737()
-    val passengerCount = Random.nextInt(1, boeing.capacity)
 
-    for (i in 0 until passengerCount){
-        val seat = boeing.getAvailableSeat() ?: return
+    val boeing = boeingType()
+    println(boeing.getInfo())
+    boeing.getSeatScheme()
+
+    println("=========================")
+
+    val zeppelin = Zeppelin()
+    val passengerCountZep = Random.nextInt(1, zeppelin.capacity)
+
+    for (i in 0 until passengerCountZep) {
+        val seat = zeppelin.getAvailableSeat() ?: return
 
         val passenger = Passenger(
             name = "Ivan",
             lastName = "Petrov",
-            passport = "${Random.nextInt(1000,9999)} ${Random.nextInt(100000,999999)}",
+            passport = "${Random.nextInt(1000, 9999)} ${Random.nextInt(100000, 999999)}",
+            seat = seat
+        )
+        zeppelin.addPassenger(passenger)
+    }
+
+    println(zeppelin.getInfo())
+    zeppelin.getSeatScheme()
+}
+
+fun boeingType(): Boeing737 {
+    val boeing = Boeing737()
+    val passengerCount = Random.nextInt(1, boeing.capacity)
+
+    for (i in 0 until passengerCount) {
+        val seat = boeing.getAvailableSeat() ?: return boeing
+
+        val passenger = Passenger(
+            name = "Ivan",
+            lastName = "Petrov",
+            passport = "${Random.nextInt(1000, 9999)} ${Random.nextInt(100000, 999999)}",
             seat = seat
         )
         boeing.addPassenger(passenger)
     }
-
-    println(boeing.getInfo())
-    boeing.getSeatScheme()
+    return boeing
 }
