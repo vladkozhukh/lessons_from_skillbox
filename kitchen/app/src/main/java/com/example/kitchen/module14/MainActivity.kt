46c95b5b081502fc8d68d84507cb544404b8e5cb
Kozhukh Vladislav <vlad32007@mail.ru>
2022-01-22 00:09:30 +0300

    add module.14.4 progress bar

--- kitchen/app/src/main/java/com/example/kitchen/module14/MainActivity.kt
+++ kitchen/app/src/main/java/com/example/kitchen/module14/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.kitchen.module14
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.Gravity
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kitchen.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,21 +20,46 @@ class MainActivity : AppCompatActivity() {
         val nameInput = findViewById<EditText>(R.id.inputText)
 
         clearButton.setOnClickListener {
-            nameInput.setText("")
-            Toast.makeText(this, "очищен текст", Toast.LENGTH_SHORT).show()
+            makeOperationOnClick()
         }
 
-        nameInput.addTextChangedListener(object : TextWatcher{
+        nameInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                nameTextView.text = s?.takeIf { it.isNotBlank() }?.let { it }
-
+                nameTextView.text = s
+                    ?.takeIf { it.isNotBlank() }
+                    ?.let { it }
                 //        кнопка.активна  = строка? {строк.поле.не_пусто} иначе ?: кнопка не активна
                 clearButton.isEnabled = s?.let { it.isNotEmpty() } ?: false
             }
+
             override fun afterTextChanged(p0: Editable?) {}
         })
+    }
 
 
+    private fun makeOperationOnClick() {
+        val progressBar = ProgressBar(this).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                gravity = when(nameText.text.toString()){
+                    "s" -> Gravity.START
+                    "e" -> Gravity.END
+                    "c" -> Gravity.CENTER
+                    "b" -> Gravity.BOTTOM
+                    else -> {Gravity.CENTER_HORIZONTAL}
+                }
+
+            }
+        }
+        container.addView(progressBar)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                container.removeView(progressBar)
+                inputText.setText("")
+            },2000
+        )
     }
 }
