249f21bb08e4b5613059ee529b8d41e169689fa6
Kozhukh Vladislav <vlad32007@mail.ru>
2022-01-23 19:06:18 +0300

    add module.15.7 Coordinator

---
+++ kitchen/app/src/main/java/com/example/kitchen/module15/CoordinatorActivity.kt
@@ -0,0 +1,7 @@
+package com.example.kitchen.module15
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kitchen.R
+
+class CoordinatorActivity: AppCompatActivity(R.layout.activity_coordinator) {
+}
